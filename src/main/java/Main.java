2406efbfc866e5d0f17221b5c3b881b261cdd40c
ort <orthodoxplus@ya.ru>
2024-05-13 22:56:00 +0300

    test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,13 +25,63 @@ public class Main {
 
 		System.out.println(dir + " created");
 
-		test2(srcFile, dir);
+		test3(srcFile, dir);
 
 		System.out.println(dir + " finished");
 		System.out.println("dir named save to clipboard");
 		copyToClipboard(now);
 	}
 
+	private static void test3(String srcFile, Path dir) {
+		FFprobeResult probe = FFprobe.probe(srcFile);
+		String frameRate = String.valueOf(probe.getVideoStream().getRFrameRate() * 2);
+		FFmpeg ffmpeg = FFmpeg.builder()
+				.printOnlyError(false)
+//				.preInputArgs(of("-fflags", "+genpts")) // для переменной частоты кадров в инпуте
+				.input(srcFile)
+				.inputDuration("120")
+				.args("-preset", "slow")
+				.args("-g", frameRate)
+				.args("-sc_threshold", "0")
+
+				.map("0:v:0")
+				.map("0:a:2")
+//				.map("0:v:0")
+//				.map("0:a:1")
+//				.map("0:s:0")
+//				.map("0:s:1")
+
+				.args("-c:v:0", "h264")
+				.args("-b:v:0", String.valueOf(probe.getVideoStream().getBitRate()))
+				.args("-c:a:0", "aac")
+				.args("-b:a:0", String.valueOf(probe.getAudioStreams().get(0).getBitRate()))
+				.args("-ac", "2") // преобразование в стерео (2 канала)
+//				.args("-c:s", "webvtt")
+
+//				.args("-strftime", "1")
+				.args("-f", "hls")
+				.args("-hls_playlist_type", "vod")
+				.args("-hls_flags", "independent_segments")
+				.args("-hls_segment_type", "mpegts")
+				.args("-master_pl_name", "master.m3u8")
+//				.args("-var_stream_map", "\"v:0,a:0,s:1 v:0,a:1,s:0\"")
+//				.args("-var_stream_map", "\"v:0,a:0 a:1\"")
+				.args("-var_stream_map", "\"v:0,a:0\"")
+
+				.args("-hls_time", "6")
+				.args("-hls_segment_filename", dir + "/index_%v/data_%d.ts")
+
+				.output(dir + "/index_%v/index.m3u8")
+				.build();
+
+		saveCommandToBatch(ffmpeg);
+	}
+
+	/**
+	 * перекодирование видео и аудио, указание битрейта
+	 * преобразование аудио в стерео
+	 * пример для входного видео с переменной частотой кадров, но кажется при перекодировании это не нужно
+	 */
 	private static void test2(String srcFile, Path dir) {
 		FFprobeResult probe = FFprobe.probe(srcFile);
 		String frameRate = String.valueOf(probe.getVideoStream().getRFrameRate() * 2);
@@ -45,7 +95,7 @@ public class Main {
 				.args("-sc_threshold", "0")
 
 				.map("0:v:0")
-				.map("0:a:0")
+				.map("0:a:2")
 //				.map("0:v:0")
 //				.map("0:a:1")
 //				.map("0:s:0")
